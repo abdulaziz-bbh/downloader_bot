2cd9bc53596a9523f6b56c08b87e0b005cffff58
abdulaziz-bbh <abdulaziz.hikmatov2001@gmail.com>
2024-12-19 12:28:24 +0500

    start

---
+++ src/test/java/com/example/downloader_bot/DownloaderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.downloader_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloaderBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
